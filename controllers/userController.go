b59108c60c641aef6d3bac30831cc98fb79e7280
Rahmat Aramadhan Husni <53205828+RahmatAramadhan@users.noreply.github.com>
2025-01-28 12:28:05 +0700

    feat: add UserController

---
+++ controllers/userController.go
@@ -0,0 +1,58 @@
+package controllers
+
+import (
+	"net/http"
+	"First-API-Golang/database"
+	"First-API-Golang/models"
+	"github.com/gin-gonic/gin"
+)
+
+func GetUsers(c *gin.Context) {
+	var users []models.User
+	if err := database.DB.Find(&users).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, users)
+}
+
+func CreateUser(c *gin.Context) {
+	var newUser models.User
+	if err := c.BindJSON(&newUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := database.DB.Create(&newUser).Error; err != nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, newUser)
+}
+
+func UpdateUser(c *gin.Context) {
+	id := c.Param("id")
+	var user models.User
+	if err := database.DB.First(&user, id).Error; err != nil{
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := database.DB.Save(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func DeleteUser(c *gin.Context){
+	id := c.Param("id")
+	if err := database.DB.Delete(&models.User{}, id).Error; err != nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "User deleted Seccessfully"})
+}
+
